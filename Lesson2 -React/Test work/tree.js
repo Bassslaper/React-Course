befaff0c9f59f56aa7e71a0ba8dca46409b3689f
Alexandr Abrosimov <mystudyjs11@gmail.com>
2020-08-21 15:30:05 +0700

    Добавил React в проект, перенс верстку в JSX

---
+++ Lesson2 -React/Test work/tree.js
@@ -0,0 +1,53 @@
+{
+  element: 'body',
+  attributes: null,
+  children: [
+    {
+      element: 'div',
+      attributes: {
+        className: 'container'
+      }
+      children: [
+          {
+            element: 'header',
+            attributes: null,
+            children: [{
+              {
+                element: 'img',
+                attributes: {
+                  src: './react-logo.svg',
+                  alt: 'logo'
+
+                },
+                children: []
+              },
+              {
+                element: 'h1',
+                attributes: null,
+                children: [{
+                  element: 'TextNode',
+                  attributes: null,
+                  children: Hello React
+                }]
+              }
+            }]
+          },
+
+          {
+            element: 'main',
+            attributes: null,
+            children: [{
+              element: 'p',
+              attributes: null,
+              children: [{
+                element: 'TextNode',
+                attributes: null,
+                children: Javascript library
+              }]
+            }]
+          }
+        
+      ]
+    }
+  ]
+}
